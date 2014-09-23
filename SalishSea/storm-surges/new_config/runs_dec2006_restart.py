625e99e78c4421dd097ec6612b7d4d05a87b1c99
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2014-09-23 14:09:39 -0700

    Modified restart run script

--- SalishSea/storm-surges/new_config/runs_dec2006_restart.py
+++ SalishSea/storm-surges/new_config/runs_dec2006_restart.py
@@ -8,28 +8,28 @@ import salishsea_cmd.api
  
  
 def main():
-    run_desc = base_run_description()
-    runs = ('all_forcing', 'tidesonly','surgeonly')
+    runs = ('all_forcing', 'tidesonly','ssh_only')
     tides= ('lateral', 'lateral.tidesonly','lateral')
     surface=('surface','surface.nosurge','surface.nosurge')
     for run_id,tide_id,surface_id in zip(runs,tides,surface):
+        run_desc = base_run_description(run_id)
         do_run(run_id, run_desc, tide_id, surface_id)
- 
+       
  
 def do_run(run_id, run_desc, tide_id, surface_id):
     run_desc['run_id'] = run_id
     run_desc['namelists'][3] = (
         'namelist.{}'.format(tide_id))
     run_desc['namelists'][2] = (
-        'namelist.{}'.format(surface_id))
+        'namelist.{}'.format(surface_id))        
     salishsea_cmd.api.run_in_subprocess(
         run_id,
         run_desc,
         'iodef_1h.xml',
-        os.path.join('/home/nksoonti/MEOPAR/SalishSea/results/storm_surges/final/dec2006/', run_id, '/restart'))
+        os.path.join('/home/nksoonti/MEOPAR/SalishSea/results/storm_surges/final/dec2006/restart',run_id))
  
  
-def base_run_description():
+def base_run_description(run_id):
 # Relative paths from SS-run-sets/SalishSea/storm_surges/new_config
     run_desc = salishsea_cmd.api.run_description(
         walltime='7:00:00',
@@ -37,7 +37,7 @@ def base_run_description():
         forcing='../../../../NEMO-forcing/',
         runs_dir='../../../../SalishSea/',
         init_conditions=(
-            os.path.join('/home/nksoonti/MEOPAR/SalishSea/results/storm_surges/final/dec2006/',run_id,'/SalishSea_00025920_restart.nc'),
+            os.path.join('/home/nksoonti/MEOPAR/SalishSea/results/storm_surges/final/dec2006/',run_id,'SalishSea_00025920_restart.nc'))
         )
     run_desc['email'] = 'nsoontie@eos.ubc.ca'
     # Relative paths to namelist section files
@@ -49,7 +49,7 @@ def base_run_description():
         'namelist.bottom',
         'namelist.tracers',
         'namelist.dynamics',
-        'namelist.compute.12x27',
+        'namelist.compute.6x14',
         ]
     return run_desc
  
