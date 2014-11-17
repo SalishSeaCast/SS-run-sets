f642713f2452598c6adbffdf185a23d4fb895098
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2014-11-16 19:38:15 -0800

    Saving run files that will reproduce paper runs

--- SalishSea/storm-surges/new_config/runs_dec2012_CGRF.py
+++ SalishSea/storm-surges/new_config/runs_dec2012_CGRF.py
@@ -9,9 +9,9 @@ import salishsea_cmd.api
  
 def main():
     run_desc = base_run_description()
-    runs = ('all_forcing','tidesonly',)
-    tides= ('lateral','lateral.tidesonly',)
-    surface=('surface','surface.nosurge',)
+    runs = ('all_forcing','tidesonly','weather_only',)
+    tides= ('lateral','lateral.tidesonly','lateral.tidesonly',)
+    surface=('surface','surface.nosurge','surface',)
     for run_id,tide_id,surface_id in zip(runs,tides,surface):
         do_run(run_id, run_desc, tide_id, surface_id)
  
