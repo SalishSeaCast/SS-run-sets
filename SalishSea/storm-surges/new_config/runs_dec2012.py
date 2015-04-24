7d0c892d27d990e67ab3ad8a52cc46aa5f2df6a7
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2015-04-24 16:39:37 -0700

    Modifications for re running

--- SalishSea/storm-surges/new_config/runs_dec2012.py
+++ SalishSea/storm-surges/new_config/runs_dec2012.py
@@ -9,9 +9,9 @@ import salishsea_cmd.api
  
 def main():
     run_desc = base_run_description()
-    runs = ('all_forcing','weather_only','no_pressure',)
-    tides= ('lateral','lateral.tidesonly','lateral.tidesonly',)
-    surface=('surface.ops','surface.ops','surface.nopressure.ops',)
+    runs = ('all_forcing',)
+    tides= ('lateral',)
+    surface=('surface.ops',)
     for run_id,tide_id,surface_id in zip(runs,tides,surface):
         do_run(run_id, run_desc, tide_id, surface_id)
  
@@ -26,7 +26,7 @@ def do_run(run_id, run_desc, tide_id, surface_id):
         run_id,
         run_desc,
         'iodef.xml',
-        os.path.join('/home/nksoonti/MEOPAR/SalishSea/results/storm_surges/final/dec2012/ops/', run_id))
+        os.path.join('/home/nksoonti/MEOPAR/SalishSea/results/storm_surges/revisions/dec2012/ops/', run_id))
  
  
 def base_run_description():
