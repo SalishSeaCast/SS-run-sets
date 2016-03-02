eec8ec38db53591f99685cb03aefae60633fb154
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2016-03-01 16:46:46 -0800

    starting run files for mixing paper simulations

---
+++ SalishSea/nancy/mixing_paper/script_runs.py
@@ -0,0 +1,61 @@
+""" Script to define and execute a series of Salish Sea NEMO model runs.
+"""
+from __future__ import absolute_import
+
+import os
+
+import salishsea_cmd.api
+
+
+def main():
+    run_desc = base_run_description()
+    run_ids = ['dwr_base',]
+    for run_id in run_ids:
+        do_run(run_id, run_desc)
+
+
+def do_run(run_id, run_desc):
+    run_desc['run_id'] = run_id
+    salishsea_cmd.api.run_in_subprocess(
+        run_id,
+        run_desc,
+        'iodef.xml',
+        os.path.join('/home/nksoonti/MEOPAR/SalishSea/results/mixing_paper',
+                     run_id))
+
+
+def base_run_description():
+    # Relative paths from SS-run-sets/SalishSea/nancy/
+    run_desc = salishsea_cmd.api.run_description(
+        walltime='21:00:00',
+        NEMO_code='/home/nksoonti/MEOPAR/NEMO-3.6-code/',
+        forcing_path='/home/nksoonti/MEOPAR/NEMO-forcing/',
+        XIOS_code='/home/nksoonti/MEOPAR/XIOS/',
+        runs_dir='/home/nksoonti/MEOPAR/SalishSea/',
+        init_conditions=(
+            '/home/dlatorne/MEOPAR/SalishSea/spin-up/'
+            '9aug18aug/'),
+        forcing={'NEMO-atmos': {'link to': '/home/sallen/MEOPAR/GEM2.5/ops/NEMO-atmos/'},
+                 'rivers': {'link to': 'rivers'},
+                 'open_boundaries': {'link to': 'open_boundaries'},
+                 'initial_strat': {'link to': 'initial_strat'},
+                 },
+        )
+    run_desc['email'] = 'nsoontie@eos.ubc.ca'
+    # Relative paths to namelist section files
+    run_desc['namelists']['namelist_cfg'] = [
+        'namelist.time.19aug28sep',
+        'namelist.domain.19aug',
+        'namelist.surface',
+        'namelist.lateral',
+        'namelist.bottom',
+        'namelist.tracers',
+        'namelist.dynamics',
+        'namelist.vertical',
+        'namelist.compute',
+        ]
+    return run_desc
+ 
+ 
+if __name__ == '__main__':
+    main()
