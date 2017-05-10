b70653fcd78ef62cf876decc8eb5cb6b22493c8f
Susan Allen <sallen@eoas.ubc.ca>
2017-05-10 16:20:22 -0600

    June 06, and Jun 16, 2015 hindcast files.

---
+++ SalishSea/nancy/dwr_new_bcs/runs_enstr.py
@@ -0,0 +1,63 @@
+""" Script to define and execute a series of Salish Sea NEMO model runs.
+All use the same RC4, corr4 files but include different numbers of tidal
+constituents
+"""
+from __future__ import absolute_import
+
+import os
+
+import salishsea_cmd.api
+
+
+def main():
+    run_desc = base_run_description()
+    run_ids = ['dwr_base_enst_eng',]# 'dwr_base_enst'],
+    laterals = [ 'namelist.lateral',]#'namelist.lateral']
+    dynamics = ['namelist.dynamics',]# 'namelist.dynamics.enst']
+    for run_id, lateral, dyn in zip(run_ids, laterals, dynamics):
+        do_run(run_id, run_desc, lateral, dyn)
+
+
+def do_run(run_id, run_desc, lateral, dyn):
+    run_desc['run_id'] = run_id
+    run_desc['namelists'][3] = lateral
+    run_desc['namelists'][6] = dyn
+    salishsea_cmd.api.run_in_subprocess(
+        run_id,
+        run_desc,
+        'iodef.eddy.1d.xml',
+        os.path.join('/home/nksoonti/MEOPAR/SalishSea/results/', run_id),
+        nemo34=True)
+
+
+def base_run_description():
+    # Relative paths from SS-run-sets/SalishSea/nancy/
+    run_desc = salishsea_cmd.api.run_description(
+        walltime='22:00:00',
+        NEMO_code='../../../../NEMO-code/',
+        forcing='../../../../NEMO-forcing/',
+        runs_dir='../../../../SalishSea/',
+        init_conditions=(
+            '/home/dlatorne/MEOPAR/SalishSea/spin-up/'
+            '11apr20apr/SalishSea_01529280_restart.nc'),
+        nemo34=True
+        )
+    run_desc['email'] = 'nsoontie@eos.ubc.ca'
+    run_desc['forcing']['atmospheric'] = '/home/nksoonti/MEOPAR/CGRF/NEMO-atmos/'
+    # Relative paths to namelist section files
+    run_desc['namelists'] = [
+        'mynamelist21apr30may.time',
+        'mynamelist.domain',
+        'namelist.surface',
+        '../tides/mynamelist.lateral',
+        '../../namelist.bottom',
+        'namelist.tracers',
+        '../../tides/mynamelist.dynamics15N',
+        '../../namelist.compute.12x27',
+        ]
+    run_desc['MPI decomposition'] = '12x27'
+    return run_desc
+ 
+ 
+if __name__ == '__main__':
+    main()
---
+++ SalishSea/storm-surges/new_config/runs_nov2006_supp.py
@@ -0,0 +1,58 @@
+"""Example script to define and execute a series of Salish Sea NEMO model runs.
+"""
+from __future__ import absolute_import
+ 
+import os
+ 
+import salishsea_cmd.api
+ 
+ 
+def main():
+    run_desc = base_run_description()
+    runs = ('no_north',)
+    tides= ('lateral.no_north_ssh',)
+    domain = ('nov2006.domain',)
+    for run_id,tide_id,domain_id in zip(runs,tides,domain):
+        do_run(run_id, run_desc, tide_id, domain_id)
+ 
+ 
+def do_run(run_id, run_desc, tide_id, domain_id):
+    run_desc['run_id'] = run_id
+    run_desc['namelists'][3] = (
+        'namelist.{}'.format(tide_id))
+    run_desc['namelists'][1] = (
+        'namelist.{}'.format(domain_id))
+    salishsea_cmd.api.run_in_subprocess(
+        run_id,
+        run_desc,
+        'iodef.xml',
+        os.path.join('/home/nksoonti/MEOPAR/SalishSea/results/storm_surges/revisions/nov2006/', run_id))
+ 
+ 
+def base_run_description():
+# Relative paths from SS-run-sets/SalishSea/storm_surges/new_config
+    run_desc = salishsea_cmd.api.run_description(
+        walltime='12:00:00',
+        NEMO_code='../../../../NEMO-code/',
+        forcing='../../../../NEMO-forcing/',
+        runs_dir='../../../../SalishSea/',
+        init_conditions=(
+            '/home/dlatorne/MEOPAR/SalishSea/spin-up/7nov16nov'),
+        )
+    run_desc['email'] = 'nsoontie@eos.ubc.ca'
+    # Relative paths to namelist section files
+    run_desc['namelists'] = [
+        'namelist.nov2006.time',
+        'namelist.nov2006.domain',
+        'namelist.surface',
+        'namelist.lateral',
+        'namelist.bottom',
+        'namelist.tracers',
+        'namelist.dynamics',
+        'namelist.compute.6x14',
+        ]
+    return run_desc
+ 
+ 
+if __name__ == '__main__':
+    main()
