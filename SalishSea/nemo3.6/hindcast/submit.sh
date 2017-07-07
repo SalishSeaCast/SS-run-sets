625a8be36c1dbf92337130556709163b1aee0d2a
Tereza <10503698+tjarnikova@users.noreply.github.com>
2017-07-07 12:03:37 -0700

    JL7

---
+++ SalishSea/nemo3.6/hindcast/submit.sh
@@ -0,0 +1,2 @@
+echo salishsea run SalishSea_hindcast_$1.yaml /home/sallen/MEOPAR/SalishSea/hindcast/$1/ --separate-deflate --nocheck-initial-conditions --waitjob $2
+salishsea run SalishSea_hindcast_$1.yaml /home/sallen/MEOPAR/SalishSea/hindcast/$1/ --separate-deflate --nocheck-initial-conditions --waitjob $2
