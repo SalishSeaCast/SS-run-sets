8b41763b5e69f5b1506b27f4a0d3b04adf89d818
Susan Allen <sallen@eoas.ubc.ca>
2017-09-23 10:19:08 -0700

    Move submit to use scratch

--- v201702/hindcast/runfiles/submit.sh
+++ v201702/hindcast/runfiles/submit.sh
@@ -1,2 +1,2 @@
-echo salishsea run SalishSea_hindcast_$1.yaml /home/sallen/SalishSea/hindcast/$1/ --separate-deflate --nocheck-initial-conditions --waitjob $2
-salishsea run SalishSea_hindcast_$1.yaml /home/sallen/SalishSea/hindcast/$1/ --separate-deflate --nocheck-initial-conditions --waitjob $2
+echo salishsea run SalishSea_hindcast_$1.yaml /global/scratch/sallen/SalishSea/hindcast/$1/ --separate-deflate --nocheck-initial-conditions --waitjob $2
+salishsea run SalishSea_hindcast_$1.yaml /global/scratch/sallen/SalishSea/hindcast/$1/ --separate-deflate --nocheck-initial-conditions --waitjob $2
