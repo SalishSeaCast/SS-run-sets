96cb66a2fb7ab939f86d747f3c3714a8906f4d5c
Elise Olson <eolson@eoas.ubc.ca>
2018-10-02 14:43:50 -0700

    runs testing ways to reduce n growth

---
+++ SS-SMELT/spring2/yamlset1new.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+NEWRUN="NoMZME"
+
+TEST="$(salishsea run spring2015_${NEWRUN}_0.yaml $SCRATCH/results/spring2015_${NEWRUN}_0 2>&1)"
+echo $TEST
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+echo $JID
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring2015_${NEWRUN}_1.yaml $SCRATCH/results/spring2015_${NEWRUN}_1 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+echo $JID
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring2015_${NEWRUN}_2.yaml $SCRATCH/results/spring2015_${NEWRUN}_2 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring2015_${NEWRUN}_3.yaml $SCRATCH/results/spring2015_${NEWRUN}_3 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring2015_${NEWRUN}_4.yaml $SCRATCH/results/spring2015_${NEWRUN}_4 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+echo "Ended run at $(date)"
+exit
