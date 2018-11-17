04e84febd88db145e5bf6c28f378c68a198cf34b
Elise Olson <eolson@eoas.ubc.ca>
2018-11-16 16:49:11 -0800

    new runset

---
+++ SS-SMELT/HC2rep/yamlset0.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NEWRUN="Z5"
+OLDRUN="Z4"
+
+#echo "working dir: $(pwd)"
+
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_0.yaml" > "spring2015_${NEWRUN}_0.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_1.yaml" > "spring15spun_${NEWRUN}_1.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_2.yaml" > "spring15spun_${NEWRUN}_2.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_3.yaml" > "spring15spun_${NEWRUN}_3.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_4.yaml" > "spring15spun_${NEWRUN}_4.yaml"
+exit
