e43d47053cedce635d13b886f985e7cecd2b3b35
Elise Olson <eolson@eoas.ubc.ca>
2018-11-17 23:35:57 -0800

    BO1 run set

--- SS-SMELT/retuneOrc/yamlset0.sh
+++ SS-SMELT/retuneOrc/yamlset0.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-NEWRUN="A02"
-OLDRUN="A01"
+NEWRUN="B01"
+OLDRUN="A02"
 
 #echo "working dir: $(pwd)"
 
-sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}new_0.yaml" > "${NEWRUN}_0.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}new_1.yaml" > "${NEWRUN}_1.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}new_2.yaml" > "${NEWRUN}_2.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}new_3.yaml" > "${NEWRUN}_3.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}new_4.yaml" > "${NEWRUN}_4.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_0.yaml" > "${NEWRUN}_0.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_1.yaml" > "${NEWRUN}_1.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_2.yaml" > "${NEWRUN}_2.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_3.yaml" > "${NEWRUN}_3.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_4.yaml" > "${NEWRUN}_4.yaml"
 exit
