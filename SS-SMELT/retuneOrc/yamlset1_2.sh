f70ecb24e2a5a4a2191f40ed8f54f5c419b4e77e
Elise Olson <eolson@eoas.ubc.ca>
2018-12-08 18:02:48 -0800

    B06

--- SS-SMELT/retuneOrc/yamlset1_2.sh
+++ SS-SMELT/retuneOrc/yamlset1_2.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-NEWRUN="B03"
+NEWRUN="orig"
 
-TEST="$(salishsea run ${NEWRUN}_m1.yaml $SCRATCH/results/${NEWRUN}_m1 2>&1)"
-echo $TEST
-if [[ $TEST != *"ERROR"* ]]; then
-   JID=${TEST##* }
-   JID=${JID%.orca2.ibb}
-else
-  exit
-fi
-echo $JID
-TEST=0
-TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_0.yaml $SCRATCH/results/${NEWRUN}_0 2>&1)"
+TEST="$(salishsea run ${NEWRUN}_1.yaml $SCRATCH/results/${NEWRUN}_1 2>&1)"
 echo $TEST
 JID=0
 if [[ $TEST != *"ERROR"* ]]; then
@@ -20,18 +10,6 @@ if [[ $TEST != *"ERROR"* ]]; then
 else
   exit
 fi
-echo $JID
-TEST=0
-TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_1.yaml $SCRATCH/results/${NEWRUN}_1 2>&1)"
-echo $TEST
-JID=0
-if [[ $TEST != *"ERROR"* ]]; then
-   JID=${TEST##* }
-   JID=${JID%.orca2.ibb}
-else
-  exit
-fi
-echo $JID
 TEST=0
 TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_2.yaml $SCRATCH/results/${NEWRUN}_2 2>&1)"
 echo $TEST
