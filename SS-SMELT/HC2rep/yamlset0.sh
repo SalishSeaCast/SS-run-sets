7d5f1629efc4ccbdf54b0b0c8d3ef8d0a701747b
Elise Olson <eolson@eoas.ubc.ca>
2018-12-12 19:17:41 -0800

    more files

--- SS-SMELT/HC2rep/yamlset0.sh
+++ SS-SMELT/HC2rep/yamlset0.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-NEWRUN="Ztest"
-OLDRUN="Z5"
+NEWRUN="Z7"
+OLDRUN="Z6"
 
 #echo "working dir: $(pwd)"
 
-sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_0.yaml" > "spring15spun_${NEWRUN}_0.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_1.yaml" > "spring15spun_${NEWRUN}_1.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_2.yaml" > "spring15spun_${NEWRUN}_2.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_3.yaml" > "spring15spun_${NEWRUN}_3.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_4.yaml" > "spring15spun_${NEWRUN}_4.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_5.yaml" > "spring15spun_${NEWRUN}_5.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_0.yaml" > "spring16spun_${NEWRUN}_0.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_1.yaml" > "spring16spun_${NEWRUN}_1.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_2.yaml" > "spring16spun_${NEWRUN}_2.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_3.yaml" > "spring16spun_${NEWRUN}_3.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_4.yaml" > "spring16spun_${NEWRUN}_4.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_5.yaml" > "spring16spun_${NEWRUN}_5.yaml"
 exit
