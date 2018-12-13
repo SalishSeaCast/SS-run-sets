02f248680e9814cc1e9edf755f47fd4e45778dc2
Elise Olson <eolson@eoas.ubc.ca>
2018-12-12 19:33:25 -0800

    newrun files

--- SS-SMELT/HC2rep/yamlset0.sh
+++ SS-SMELT/HC2rep/yamlset0.sh
@@ -4,10 +4,10 @@ OLDRUN="Z6"
 
 #echo "working dir: $(pwd)"
 
-sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_0.yaml" > "spring16spun_${NEWRUN}_0.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_1.yaml" > "spring16spun_${NEWRUN}_1.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_2.yaml" > "spring16spun_${NEWRUN}_2.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_3.yaml" > "spring16spun_${NEWRUN}_3.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_4.yaml" > "spring16spun_${NEWRUN}_4.yaml"
-sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_5.yaml" > "spring16spun_${NEWRUN}_5.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_20.yaml" > "spring16spun_${NEWRUN}_0.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_21.yaml" > "spring16spun_${NEWRUN}_1.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_22.yaml" > "spring16spun_${NEWRUN}_2.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_23.yaml" > "spring16spun_${NEWRUN}_3.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_24.yaml" > "spring16spun_${NEWRUN}_4.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "spring15spun_${OLDRUN}_25.yaml" > "spring16spun_${NEWRUN}_5.yaml"
 exit
--- SS-SMELT/HC2rep/yamlset1.sh
+++ SS-SMELT/HC2rep/yamlset1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-NEWRUN="Ztest"
+NEWRUN="Z7"
 
-TEST="$(salishsea run spring15spun_${NEWRUN}_0.yaml $SCRATCH/results/spring15spun_${NEWRUN}_0 2>&1)"
+TEST="$(salishsea run spring16spun_${NEWRUN}_0.yaml $SCRATCH/results/spring16spun_${NEWRUN}_0 2>&1)"
 echo $TEST
 if [[ $TEST != *"ERROR"* ]]; then
    JID=${TEST##* }
@@ -10,7 +10,7 @@ else
 fi
 echo $JID
 TEST=0
-TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring15spun_${NEWRUN}_1.yaml $SCRATCH/results/spring15spun_${NEWRUN}_1 2>&1)"
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring16spun_${NEWRUN}_1.yaml $SCRATCH/results/spring16spun_${NEWRUN}_1 2>&1)"
 echo $TEST
 JID=0
 if [[ $TEST != *"ERROR"* ]]; then
@@ -20,7 +20,7 @@ else
 fi
 echo $JID
 TEST=0
-TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring15spun_${NEWRUN}_2.yaml $SCRATCH/results/spring15spun_${NEWRUN}_2 2>&1)"
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring16spun_${NEWRUN}_2.yaml $SCRATCH/results/spring16spun_${NEWRUN}_2 2>&1)"
 echo $TEST
 JID=0
 if [[ $TEST != *"ERROR"* ]]; then
@@ -29,7 +29,7 @@ else
   exit
 fi
 TEST=0
-TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring15spun_${NEWRUN}_3.yaml $SCRATCH/results/spring15spun_${NEWRUN}_3 2>&1)"
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring16spun_${NEWRUN}_3.yaml $SCRATCH/results/spring16spun_${NEWRUN}_3 2>&1)"
 echo $TEST
 JID=0
 if [[ $TEST != *"ERROR"* ]]; then
@@ -38,7 +38,7 @@ else
   exit
 fi
 TEST=0
-TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring15spun_${NEWRUN}_4.yaml $SCRATCH/results/spring15spun_${NEWRUN}_4 2>&1)"
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring16spun_${NEWRUN}_4.yaml $SCRATCH/results/spring16spun_${NEWRUN}_4 2>&1)"
 echo $TEST
 JID=0
 if [[ $TEST != *"ERROR"* ]]; then
@@ -47,7 +47,7 @@ else
   exit
 fi
 TEST=0
-TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring15spun_${NEWRUN}_5.yaml $SCRATCH/results/spring15spun_${NEWRUN}_5 2>&1)"
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions spring16spun_${NEWRUN}_5.yaml $SCRATCH/results/spring16spun_${NEWRUN}_5 2>&1)"
 echo $TEST
 JID=0
 if [[ $TEST != *"ERROR"* ]]; then
