63dc073860024cc2724558e8357aab11cf8ce1c6
Elise Olson <eolson@eoas.ubc.ca>
2019-01-18 17:05:05 -0800

    change file

--- SS-SMELT/GLSOut/batchDeflate.sh
+++ SS-SMELT/GLSOut/batchDeflate.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-#SBATCH --job-name=01feb15_finish
+#SBATCH --job-name=deflate
 #SBATCH --constraint=skylake
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=48
 #SBATCH --mem=0
-#SBATCH --time=6:00:00
-#SBATCH --mail-user=sallen@eoas.ubc.ca
+#SBATCH --time=3:00:00
+#SBATCH --mail-user=eolson@eoas.ubc.ca
 #SBATCH --mail-type=ALL
 #SBATCH --account=def-allen
 # stdout and stderr file paths/names
-#SBATCH --output=/scratch/allen/hindcast_v201812/01feb15/stdout_finish
-#SBATCH --error=/scratch/allen/hindcast_v201812/01feb15/stderr_finish
+#SBATCH --output=/scratch/eolson/results/GLSsummer15_0/stdout_deflate
+#SBATCH --error=/scratch/eolson/results/GLSsummer15_0/stderr_deflate
 
 
-RUN_ID="01feb15_hindcast"
-RUN_DESC="01feb15hindcast.yaml"
-RESULTS_DIR="/scratch/allen/hindcast_v201812/01feb15"
+RUN_ID="GLSsummer15_0"
+RUN_DESC="GLSsummer15_0.yaml"
+RESULTS_DIR="/scratch/eolson/results/GLSsummer15_0"
 DEFLATE="${HOME}/.local/bin/salishsea deflate"
 
 module load netcdf-fortran-mpi/4.4.4
@@ -24,5 +24,5 @@ module load python/3.7.0
 
 echo "Results deflation started at $(date)"
 module load nco/4.6.6
-${DEFLATE} *_turb_T*.nc --jobs 48 --debug
+${DEFLATE} SalishSea_1h*.nc --jobs 48 --debug
 echo "Results deflation ended at $(date)"
