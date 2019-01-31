94bcad730589e8f5159032c3fbb8f111f22d6a21
Elise Olson <eolson@eos.ubc.ca>
2019-01-31 11:04:18 -0800

    fix var name

--- SS-SMELT/HC1612Tune/batchDeflate.sh
+++ SS-SMELT/HC1612Tune/batchDeflate.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 #SBATCH --job-name=deflate
-#SBATCH --time=3:00:00
+#SBATCH --mem=0
+#SBATCH --time=1:00:00
 #SBATCH --mail-user=eolson@eoas.ubc.ca
 #SBATCH --mail-type=ALL
 #SBATCH --account=def-allen
 # stdout and stderr file paths/names
-#SBATCH --output=/scratch/eolson/results/GLSsummer15_0/stdout_deflate
-#SBATCH --error=/scratch/eolson/results/GLSsummer15_0/stderr_deflate
+#SBATCH --output=/scratch/eolson/results/spring15_R3_0/stdout_deflate
+#SBATCH --error=/scratch/eolson/results/spring15_R3_0/stderr_deflate
 
-PATH=$/scratch/eolson/results/spring15_R2_0/
+LOC1=/scratch/eolson/results/spring15_R3_0/
 
 echo "started at $(date)"
 module load nco/4.6.6
 
-for i in ${PATH}_SalishSea_1h_*.nc
+for i in ${LOC1}_SalishSea_1h_*.nc
 do
    filenew=$(echo $i|cut -d'.' -f 1)
    nccopy -s -4 -d4 $i $filenew'_sm.nc'
