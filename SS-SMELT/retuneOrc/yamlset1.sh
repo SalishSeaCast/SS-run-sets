ad158583274bb9230b66f4ee9a75827a16ad5dea
Ben Moore-Maley <18601938+bmooremaley@users.noreply.github.com>
2019-02-15 16:18:55 -0800

    Added more analytical u wind to Salish Sea pond simulations with lake bathymetry

---
+++ SS-SMELT/retuneOrc/yamlset1.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+NEWRUN="C07"
+
+TEST="$(salishsea run ${NEWRUN}_m2.yaml $SCRATCH/results/${NEWRUN}_m2 2>&1)"
+echo $TEST
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+   JID=${JID%.orca2.ibb}
+else
+  exit
+fi
+echo $JID
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_m1.yaml $SCRATCH/results/${NEWRUN}_m1 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+   JID=${JID%.orca2.ibb}
+else
+  exit
+fi
+echo $JID
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_0.yaml $SCRATCH/results/${NEWRUN}_0 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+   JID=${JID%.orca2.ibb}
+else
+  exit
+fi
+echo $JID
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_1.yaml $SCRATCH/results/${NEWRUN}_1 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+   JID=${JID%.orca2.ibb}
+else
+  exit
+fi
+echo $JID
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_2.yaml $SCRATCH/results/${NEWRUN}_2 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+   JID=${JID%.orca2.ibb}
+else
+  exit
+fi
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_3.yaml $SCRATCH/results/${NEWRUN}_3 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+   JID=${JID%.orca2.ibb}
+else
+  exit
+fi
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_4.yaml $SCRATCH/results/${NEWRUN}_4 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+   JID=${JID%.orca2.ibb}
+else
+  exit
+fi
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_5.yaml $SCRATCH/results/${NEWRUN}_5 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+   JID=${JID%.orca2.ibb}
+else
+  exit
+fi
+echo "Ended run at $(date)"
+exit
---
+++ SS-SMELT/spring2015/yamlsetrst.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+NEWRUN="slowPP3HI"
+TEST="$(salishsea run --skylake spring2015_${NEWRUN}_1rst.yaml $SCRATCH/results/spring2015_${NEWRUN}_1 2>&1)"
+echo $TEST
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+echo $JID
+TEST=0
+TEST="$(salishsea run --skylake --waitjob ${JID} --nocheck-initial-conditions spring2015_${NEWRUN}_2.yaml $SCRATCH/results/spring2015_${NEWRUN}_2 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+TEST=0
+TEST="$(salishsea run --skylake --waitjob ${JID} --nocheck-initial-conditions spring2015_${NEWRUN}_3.yaml $SCRATCH/results/spring2015_${NEWRUN}_3 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+TEST=0
+TEST="$(salishsea run --skylake --waitjob ${JID} --nocheck-initial-conditions spring2015_${NEWRUN}_4.yaml $SCRATCH/results/spring2015_${NEWRUN}_4 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+echo "Done at $(date)"
+exit
