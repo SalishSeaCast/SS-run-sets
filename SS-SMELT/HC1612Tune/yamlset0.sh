184ce9979f1efeb5981788e41f455b2452abe019
Elise Olson <eolson@eoas.ubc.ca>
2019-03-04 18:19:23 -0800

    run GLS output for summer 15

--- SS-SMELT/HC1612Tune/yamlset0.sh
+++ SS-SMELT/HC1612Tune/yamlset0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NEWRUN="R4NoSiT"
+NEWRUN="R"
 OLDRUN="R3"
 PREFIX=spring15
 
@@ -9,4 +9,6 @@ sed "s/$OLDRUN/$NEWRUN/" "${PREFIX}_${OLDRUN}_0.yaml" > "${PREFIX}_${NEWRUN}_0.y
 sed "s/$OLDRUN/$NEWRUN/" "${PREFIX}_${OLDRUN}_1.yaml" > "${PREFIX}_${NEWRUN}_1.yaml"
 sed "s/$OLDRUN/$NEWRUN/" "${PREFIX}_${OLDRUN}_2.yaml" > "${PREFIX}_${NEWRUN}_2.yaml"
 sed "s/$OLDRUN/$NEWRUN/" "${PREFIX}_${OLDRUN}_3.yaml" > "${PREFIX}_${NEWRUN}_3.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "${PREFIX}_${OLDRUN}_4.yaml" > "${PREFIX}_${NEWRUN}_4.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "${PREFIX}_${OLDRUN}_5.yaml" > "${PREFIX}_${NEWRUN}_5.yaml"
 exit
