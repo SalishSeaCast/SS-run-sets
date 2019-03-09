48f2664339b9769d619b146e5bd5000e1a247c63
Elise Olson <eolson@eos.ubc.ca>
2019-03-08 17:22:13 -0800

    new stuff in GLS diagnostic runs

--- SS-SMELT/GLSOut/batchDeflate.sh
+++ SS-SMELT/GLSOut/batchDeflate.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
-
 #SBATCH --job-name=deflate
-#SBATCH --constraint=skylake
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=48
-#SBATCH --mem=0
-#SBATCH --time=3:00:00
+#SBATCH --mem=2000M
+#SBATCH --time=6:00:00
 #SBATCH --mail-user=eolson@eoas.ubc.ca
 #SBATCH --mail-type=ALL
 #SBATCH --account=def-allen
 # stdout and stderr file paths/names
-#SBATCH --output=/scratch/eolson/results/GLSsummer15_0/stdout_deflate
-#SBATCH --error=/scratch/eolson/results/GLSsummer15_0/stderr_deflate
+#SBATCH --output=/scratch/eolson/results/stdout_deflate
+#SBATCH --error=/scratch/eolson/results/stderr_deflate
 
+LOC1=/scratch/eolson/results/GLS_Summer15_1b/
 
-RUN_ID="GLSsummer15_0"
-RUN_DESC="GLSsummer15_0.yaml"
-RESULTS_DIR="/scratch/eolson/results/GLSsummer15_0"
-DEFLATE="${HOME}/.local/bin/salishsea deflate"
-
-module load netcdf-fortran-mpi/4.4.4
-module load python/3.7.0
-
-echo "Results deflation started at $(date)"
+echo "started at $(date)"
 module load nco/4.6.6
-${DEFLATE} SalishSea_1h*.nc --jobs 48 --debug
+
+for i in ${LOC1}SalishSea_1h_*.nc
+do
+   filenew=$(echo $i|cut -d'.' -f 1)
+   nccopy -s -4 -d4 $i $filenew'_sm.nc'
+done
+
 echo "Results deflation ended at $(date)"
