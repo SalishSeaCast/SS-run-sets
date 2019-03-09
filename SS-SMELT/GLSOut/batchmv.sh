888c8a12c56bfee1bd0db6f6d06b46e220713ea1
Elise Olson <eolson@eoas.ubc.ca>
2019-03-08 17:22:13 -0800

    new stuff in GLS diagnostic runs

---
+++ SS-SMELT/GLSOut/batchmv.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+LOC1=/scratch/eolson/results/GLS_Summer15_1/
+
+echo "started at $(date)"
+module load nco/4.6.6
+
+for i in ${LOC1}*_sm.nc
+do
+   s=${i%_sm.nc}; 
+   mv $i ${s}.nc;
+done
+
+echo "Results deflation ended at $(date)"
