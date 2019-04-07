97dca624ba05c240489734c51c80b319f7798992
Elise Olson <eolson@eos.ubc.ca>
2019-04-07 00:39:44 -0700

    new run

--- SS-SMELT/NewPAR/batchDeflateTemp.sh
+++ SS-SMELT/NewPAR/batchDeflateTemp.sh
@@ -9,12 +9,14 @@
 #SBATCH --output=/scratch/eolson/results/stdout_deflate
 #SBATCH --error=/scratch/eolson/results/stderr_deflate
 
-LOC1=/scratch/eolson/results/PAR1_3_0/
+LOC1=/scratch/eolson/results/PAR1_3_3/
 
 echo "started at $(date)"
 module load nco/4.6.6
 
-nccopy -s -4 -d4 /scratch/eolson/results/PAR1_3_1/SalishSea_1h_20170220_20170410_ptrc_T_20170322-20170331.nc /scratch/eolson/results/PAR1_3_1/SalishSea_1h_20170220_20170410_ptrc_T_20170322-20170331_sm.nc
-nccopy -s -4 -d4 /scratch/eolson/results/PAR1_3_1/SalishSea_1h_20170220_20170410_ptrc_T_20170401-20170410.nc /scratch/eolson/results/PAR1_3_1/SalishSea_1h_20170220_20170410_ptrc_T_20170401-20170410_sm.nc
+nccopy -s -4 -d4 /scratch/eolson/results/PAR1_3_3/SalishSea_1h_20170411_20170530_ptrc_T_20170421-20170430.nc /scratch/eolson/results/PAR1_3_3/SalishSea_1h_20170411_20170530_ptrc_T_20170421-20170430_sm.nc
+nccopy -s -4 -d4 /scratch/eolson/results/PAR1_3_3/SalishSea_1h_20170411_20170530_ptrc_T_20170501-20170510.nc /scratch/eolson/results/PAR1_3_3/SalishSea_1h_20170411_20170530_ptrc_T_20170501-20170510_sm.nc
+nccopy -s -4 -d4 /scratch/eolson/results/PAR1_3_3/SalishSea_1h_20170411_20170530_ptrc_T_20170511-20170520.nc /scratch/eolson/results/PAR1_3_3/SalishSea_1h_20170411_20170530_ptrc_T_20170511-20170520_sm.nc
+nccopy -s -4 -d4 /scratch/eolson/results/PAR1_3_3/SalishSea_1h_20170411_20170530_ptrc_T_20170521-20170530.nc /scratch/eolson/results/PAR1_3_3/SalishSea_1h_20170411_20170530_ptrc_T_20170521-20170530_sm.nc
 
 echo "Results deflation ended at $(date)"
