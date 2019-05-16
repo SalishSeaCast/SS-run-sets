2bbcbae39796a21a9256b62f8e547735827356b6
Elise Olson <eolson@eoas.ubc.ca>
2019-05-16 15:29:43 -0700

    new test run with sinkingdep on surf bsi

--- SS-SMELT/NewPAR/batchDeflate3b.sh
+++ SS-SMELT/NewPAR/batchDeflate3b.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=deflate
-#SBATCH --dependency=afterok:20186663
+#SBATCH --dependency=afterok:20316329
 #SBATCH --mem=2000M
-#SBATCH --time=3:00:00
+#SBATCH --time=6:00:00
 #SBATCH --mail-user=eolson@eoas.ubc.ca
 #SBATCH --mail-type=ALL
 #SBATCH --account=rrg-allen
@@ -10,12 +10,12 @@
 #SBATCH --output=/scratch/eolson/results/stdout_deflate
 #SBATCH --error=/scratch/eolson/results/stderr_deflate
 
-LOC1=/scratch/eolson/results/PAR_13bfastSiLR_3/
+LOC1=/scratch/eolson/results/PAR_13bfastSiHR_2/
 
 echo "started at $(date)"
 module load nco/4.6.6
 
-for i in ${LOC1}SalishSea_1h_*prod*20170[57]*-2017*.nc
+for i in ${LOC1}SalishSea_1h_*.nc
 do
    filenew=$(echo $i|cut -d'.' -f 1)
    nccopy -s -4 -d4 $i $filenew'_sm.nc'
--- SS-SMELT/PARBSI/batchDeflate3.sh
+++ SS-SMELT/PARBSI/batchDeflate3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=deflate
+#SBATCH --dependency=afterok:20186663
 #SBATCH --mem=2000M
 #SBATCH --time=12:00:00
 #SBATCH --mail-user=eolson@eoas.ubc.ca
@@ -9,26 +10,15 @@
 #SBATCH --output=/scratch/eolson/results/stdout_deflate
 #SBATCH --error=/scratch/eolson/results/stderr_deflate
 
-LOC1=/scratch/eolson/results/PAR_13bfastSiHR_3/
+LOC1=/scratch/eolson/results/PAR_13bfastSiLR_3/
 
 echo "started at $(date)"
 module load nco/4.6.6
 
-for i in ${LOC1}SalishSea_1h_*ptrc*7??.nc
+for i in ${LOC1}SalishSea_1h_*.nc
 do
    filenew=$(echo $i|cut -d'.' -f 1)
    nccopy -s -4 -d4 $i $filenew'_sm.nc'
 done
-
-for i in ${LOC1}SalishSea_1h_*ptrc*629.nc
-do
-   filenew=$(echo $i|cut -d'.' -f 1)
-   nccopy -s -4 -d4 $i $filenew'_sm.nc'
-done
-#for i in ${LOC1}SalishSea_1h_*prod*05[23]0.nc
-#do
-#   filenew=$(echo $i|cut -d'.' -f 1)
-#   nccopy -s -4 -d4 $i $filenew'_sm.nc'
-#done
 
 echo "Results deflation ended at $(date)"
