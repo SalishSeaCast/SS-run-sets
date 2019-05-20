9df93457f77250a834086178bf10c44dfaa41912
Elise Olson <eolson@eos.ubc.ca>
2019-05-19 23:14:03 -0700

    new LO run

---
+++ SS-SMELT/NewLO/yamlset1.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+NEWRUN=NewLoHiSi
+
+TEST="$(salishsea run ${NEWRUN}_0.yaml $SCRATCH/results/${NEWRUN}_0 2>&1)"
+echo $TEST
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+echo $JID
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_1.yaml $SCRATCH/results/${NEWRUN}_1 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+echo $JID
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_2.yaml $SCRATCH/results/${NEWRUN}_2 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+echo $JID
+TEST=0
+TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_3.yaml $SCRATCH/results/${NEWRUN}_3 2>&1)"
+echo $TEST
+JID=0
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+echo $JID
+#TEST=0
+#TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_4.yaml $SCRATCH/results/${NEWRUN}_4 2>&1)"
+#echo $TEST
+#JID=0
+#if [[ $TEST != *"ERROR"* ]]; then
+#   JID=${TEST##* }
+#else
+#  exit
+#fi
+#echo $JID
+#TEST=0
+#TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_5.yaml $SCRATCH/results/${NEWRUN}_5 2>&1)"
+#echo $TEST
+#JID=0
+#if [[ $TEST != *"ERROR"* ]]; then
+#   JID=${TEST##* }
+#else
+#  exit
+#fi
+#echo $JID
+echo "Ended run at $(date)"
+exit
