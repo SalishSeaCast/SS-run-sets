fa1862bfeb5771a9d732b0f2bb9ebb574b36c6a5
Elise Olson <eolson@eos.ubc.ca>
2019-05-31 09:53:54 -0700

    improved bash scripts in NewLO

--- SS-SMELT/NewLO/yamlset1.sh
+++ SS-SMELT/NewLO/yamlset1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NEWRUN=NewLONoSiT
 
-TEST="$(salishsea run ${NEWRUN}_0.yaml $SCRATCH/results/${NEWRUN}_1 2>&1)"
+TEST="$(salishsea run ${NEWRUN}_0.yaml $SCRATCH/results/${NEWRUN}_0 2>&1)"
 echo $TEST
 if [[ $TEST != *"ERROR"* ]]; then
    JID=${TEST##* }
@@ -9,36 +9,41 @@ else
   exit
 fi
 echo $JID
-TEST=0
-TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_1.yaml $SCRATCH/results/${NEWRUN}_1 2>&1)"
-echo $TEST
-JID=0
-if [[ $TEST != *"ERROR"* ]]; then
-   JID=${TEST##* }
-else
-  exit
-fi
-echo $JID
-TEST=0
-TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_2.yaml $SCRATCH/results/${NEWRUN}_2 2>&1)"
-echo $TEST
-JID=0
-if [[ $TEST != *"ERROR"* ]]; then
-   JID=${TEST##* }
-else
-  exit
-fi
-echo $JID
-TEST=0
-TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_3.yaml $SCRATCH/results/${NEWRUN}_3 2>&1)"
-echo $TEST
-JID=0
-if [[ $TEST != *"ERROR"* ]]; then
-   JID=${TEST##* }
-else
-  exit
-fi
-echo $JID
+sbatch --dependency=afterok:${JID} batchDeflate.sh ${NEWRUN} 0
+
+for j in $(seq 1 3); do
+  TEST=0
+  TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_${j}.yaml $SCRATCH/results/${NEWRUN}_${j} 2>&1)"
+  echo $TEST
+  JID=0
+  if [[ $TEST != *"ERROR"* ]]; then
+     JID=${TEST##* }
+  else
+    exit
+  fi
+  echo $JID
+  sbatch --dependency=afterok:${JID} batchDeflate.sh $NEWRUN $j
+done
+#TEST=0
+#TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_2.yaml $SCRATCH/results/${NEWRUN}_2 2>&1)"
+#echo $TEST
+#JID=0
+#if [[ $TEST != *"ERROR"* ]]; then
+#   JID=${TEST##* }
+#else
+#  exit
+#fi
+#echo $JID
+#TEST=0
+#TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_3.yaml $SCRATCH/results/${NEWRUN}_3 2>&1)"
+#echo $TEST
+#JID=0
+#if [[ $TEST != *"ERROR"* ]]; then
+#   JID=${TEST##* }
+#else
+#  exit
+#fi
+#echo $JID
 #TEST=0
 #TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_4.yaml $SCRATCH/results/${NEWRUN}_4 2>&1)"
 #echo $TEST
