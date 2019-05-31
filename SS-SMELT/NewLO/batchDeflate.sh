061c3e187c9c114ce7fa0823d06a9032615207f8
Elise Olson <eolson@eoas.ubc.ca>
2019-05-31 09:53:54 -0700

    improved bash scripts in NewLO

--- SS-SMELT/NewLO/batchDeflate.sh
+++ SS-SMELT/NewLO/batchDeflate.sh
@@ -9,13 +9,16 @@
 #SBATCH --output=/scratch/eolson/results/stdout_deflate
 #SBATCH --error=/scratch/eolson/results/stderr_deflate
 
-LOC1=/scratch/eolson/results/NewLoHiSi_3/
+LOC1=/scratch/eolson/results/${1}_${2}/
 
 echo "started at $(date)"
 module load nco/4.6.6
 
+echo ${LOC1}
+
 for i in ${LOC1}SalishSea_1h_*.nc
 do
+   echo $i
    filenew=$(echo $i|cut -d'.' -f 1)
    nccopy -s -4 -d4 $i $filenew'_sm.nc'
 done
