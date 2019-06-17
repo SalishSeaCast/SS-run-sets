c5368571cb94f15e69bb465d28fd4fba5ff0b2fc
Elise Olson <eolson@eos.ubc.ca>
2019-06-17 13:45:18 -0700

    NewLOGnSiT run

--- SS-SMELT/NewLO/yamlset0.sh
+++ SS-SMELT/NewLO/yamlset0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-NEWRUN="NewLONoSiT"
-OLDRUN="NewLoHiSi"
+NEWRUN="NewLOGnSiT"
+OLDRUN="NewLONoSiT"
 
 #echo "working dir: $(pwd)"
 
@@ -8,6 +8,6 @@ sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_0.yaml" > "${NEWRUN}_0.yaml"
 sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_1.yaml" > "${NEWRUN}_1.yaml"
 sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_2.yaml" > "${NEWRUN}_2.yaml"
 sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_3.yaml" > "${NEWRUN}_3.yaml"
-#sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_4.yaml" > "${NEWRUN}_4.yaml"
-#sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_5.yaml" > "${NEWRUN}_5.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_4.yaml" > "${NEWRUN}_4.yaml"
+sed "s/$OLDRUN/$NEWRUN/" "${OLDRUN}_5.yaml" > "${NEWRUN}_5.yaml"
 exit
