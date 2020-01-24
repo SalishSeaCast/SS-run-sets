553aed33466ab8e3525847793341dfbfc289d933
Elise Olson <eolson@eoas.ubc.ca>
2020-01-24 17:30:37 -0500

    rerun

--- SS-SMELT/GLSOut/yamlset_noMrubrum.sh
+++ SS-SMELT/GLSOut/yamlset_noMrubrum.sh
@@ -2,6 +2,16 @@
 NEWRUN=noMrubrum
 PREFIX=rev
 
+TEST="$(salishsea run ${PREFIX}_${NEWRUN}_0.yaml $SCRATCH/results/${PREFIX}_${NEWRUN}_0 2>&1)"
+echo $TEST
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+echo $JID
+echo "Ended run at $(date)"
+TEST=0
 TEST="$(salishsea run ${PREFIX}_${NEWRUN}_1.yaml $SCRATCH/results/${PREFIX}_${NEWRUN}_1 2>&1)"
 echo $TEST
 if [[ $TEST != *"ERROR"* ]]; then
