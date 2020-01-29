adfc66061fa73c2ddca3506fa267aa5d5cd6bed4
Elise Olson <eolson@eos.ubc.ca>
2020-01-29 01:22:37 -0500

    fix yaml mistake

--- SS-SMELT/noDisc/yamlset_noDisc.sh
+++ SS-SMELT/noDisc/yamlset_noDisc.sh
@@ -2,17 +2,7 @@
 NEWRUN=noDisc
 PREFIX=rev
 
-TEST="$(salishsea run ${PREFIX}_${NEWRUN}_0.yaml $SCRATCH/results/${PREFIX}_${NEWRUN}_0 2>&1)"
-echo $TEST
-if [[ $TEST != *"ERROR"* ]]; then
-   JID=${TEST##* }
-else
-  exit
-fi
-echo $JID
-echo "Ended run at $(date)"
-TEST=0
-TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${PREFIX}_${NEWRUN}_1.yaml $SCRATCH/results/${PREFIX}_${NEWRUN}_1 2>&1)"
+TEST="$(salishsea run ${PREFIX}_${NEWRUN}_1.yaml $SCRATCH/results/${PREFIX}_${NEWRUN}_1 2>&1)"
 echo $TEST
 if [[ $TEST != *"ERROR"* ]]; then
    JID=${TEST##* }
