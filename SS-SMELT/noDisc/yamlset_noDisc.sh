16d37d4406c3de13d284ce35984fc2ea111cf35d
Elise Olson <eolson@eos.ubc.ca>
2020-02-03 02:34:54 -0500

    run with shorter time step

--- SS-SMELT/noDisc/yamlset_noDisc.sh
+++ SS-SMELT/noDisc/yamlset_noDisc.sh
@@ -2,18 +2,8 @@
 NEWRUN=noDisc
 PREFIX=rev
 
-TEST="$(salishsea run ${PREFIX}_${NEWRUN}_1.yaml $SCRATCH/results/${PREFIX}_${NEWRUN}_1 2>&1)"
+TEST="$(salishsea run ${PREFIX}_${NEWRUN}_2.yaml $SCRATCH/results/${PREFIX}_${NEWRUN}_2 2>&1)"
 echo $TEST
-if [[ $TEST != *"ERROR"* ]]; then
-   JID=${TEST##* }
-else
-  exit
-fi
-echo $JID
-TEST=0
-TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${PREFIX}_${NEWRUN}_2.yaml $SCRATCH/results/${PREFIX}_${NEWRUN}_2 2>&1)"
-echo $TEST
-JID=0
 if [[ $TEST != *"ERROR"* ]]; then
    JID=${TEST##* }
 else
