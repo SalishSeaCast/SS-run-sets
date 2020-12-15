ca936d049f1396ebd93be09d5d9dbb8876e7b22a
e-olson <eolson@eoas.ubc.ca>
2020-12-14 22:04:08 -0800

    yamlset version wtih arguments

---
+++ TRACE3/yamlset.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+NEWRUN=$1
+NSTART=$2
+NEND=$3
+
+TEST="$(salishsea run ${NEWRUN}_${NSTART}.yaml $SCRATCH/results/${NEWRUN} 2>&1)"
+echo $TEST
+if [[ $TEST != *"ERROR"* ]]; then
+   JID=${TEST##* }
+else
+  exit
+fi
+echo $JID
+
+NSTART=$(($NSTART+1))
+
+for j in $(seq $NSTART $NEND); do
+  TEST=0
+  TEST="$(salishsea run --waitjob ${JID} --nocheck-initial-conditions ${NEWRUN}_${j}.yaml $SCRATCH/results/${NEWRUN} 2>&1)"
+  echo $TEST
+  JID=0
+  if [[ $TEST != *"ERROR"* ]]; then
+     JID=${TEST##* }
+  else
+    exit
+  fi
+  echo $JID
+done
